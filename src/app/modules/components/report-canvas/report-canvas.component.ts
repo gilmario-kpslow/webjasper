77afa35727a22e1a67661c4ff69ddfb27547e60a
Gilm´ário Batista <gilmariosoftware@gmail.com>
2023-04-10 15:49:03 +0000

    Ajustes

--- src/app/modules/components/report-canvas/report-canvas.component.ts
+++ src/app/modules/components/report-canvas/report-canvas.component.ts
@@ -1,20 +1,27 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-report-canvas',
   templateUrl: './report-canvas.component.html',
   styleUrls: ['./report-canvas.component.css']
 })
-export class ReportCanvasComponent {
+export class ReportCanvasComponent implements AfterViewInit{
 
   canvas: HTMLCanvasElement| undefined;
   context: CanvasRenderingContext2D| null | undefined;
 
+  @ViewChild("main") div: ElementRef| undefined
+
   constructor(private renderer: Renderer2,private el: ElementRef) {
-    this.canvas = renderer.createElement('canvas');
-    this.context = this.canvas?.getContext('2d');
-    renderer.appendChild(el.nativeElement, this.canvas);
+    
   }
+
+ngAfterViewInit(): void {
+  console.log(this.div)
+    this.canvas = this.renderer.createElement('canvas');
+    this.context = this.canvas?.getContext('2d');
+    this.renderer.appendChild(this.div?.nativeElement, this.canvas);
+}
 
   setDimensoes(width: number, heigth: number) {
     this.renderer.setAttribute( this.canvas,'width', `${width}`);
